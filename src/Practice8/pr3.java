7e74900063bdec62b18547fba0fd0acf20019eb1
eunyoung <whdmsdud77@naver.com>
2023-05-28 15:06:11 +0900

    네번째 커밋

--- src/Practice8/pr3.java
+++ src/Practice8/pr3.java
@@ -1,10 +1,5 @@
 package Practice8;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-
 public class pr3 {
 
     public static void main(String[] args) {
@@ -12,30 +7,14 @@ public class pr3 {
     }
     public static int solution(String s) {
         int answer = 0;
-        Map<String, String> map = new HashMap<>();
-        map.put("0", "zero");
-        map.put("1", "one");
-        map.put("2", "two");
-        map.put("3", "three");
-        map.put("4", "four");
-        map.put("5", "five");
-        map.put("6", "six");
-        map.put("7", "seven");
-        map.put("8", "eight");
-        map.put("9", "nine");
 
-        String[] arr = s.split("");
-        System.out.println(Arrays.toString(arr));
+        String[] arr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
 
-        ArrayList<String> newArr = new ArrayList<>();
-
-        for(int i = 0; i < s.length(); i++) {
-//            if(newArr.add(arr[i].equals(map.())))
-
-            if(arr[i].equals(map.containsKey(map.get(i)))) {
-
-            }
+        for(int i = 0; i < 10; i++) {
+            s = s.replace(arr[i], Integer.toString(i));
         }
+        System.out.println(s);
+        answer = Integer.parseInt(s);
 
         return answer;
     }
